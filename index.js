45880b930be8285e2a30b5976e332dcfaf620cc0
PritrirajPartho <dapartho5616@gmail.com>
2023-03-04 23:53:30 +0600

    last-added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
+//step-1111111........
 const dataLoading = () =>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
-    .then(data => displayDetails(data.data.tools));
+    .then(data => displayDetails(data.data.tools));   
 }
 dataLoading();
 
@@ -9,6 +11,8 @@ dataLoading();
 const displayDetails = openapis =>{
     //console.log(openapis);
     const apisContainer = document.getElementById('apis-container');
+    const btnSeeMore = document.getElementById('btn-see-more');
+    // see more option condition...........
     openapis.forEach(openapi => {
         //  console.log(openapi);
         const apiDiv = document.createElement('div');
@@ -39,10 +43,13 @@ const displayDetails = openapis =>{
                 </div>
         `
         apisContainer.appendChild(apiDiv);
-   })
-}
+    })
+     
+    
+ }
 
-/* id onujaye details dekanu */
+
+/* id onujayee details dekanu */
 const loadApiDetail = apiId =>{
     const url = `https://openapi.programming-hero.com/api/ai/tool/${apiId}`;
     console.log(url);
@@ -56,17 +63,17 @@ const OpenApiDetails = api =>{
     console.log(api);
     document.getElementById('api-description').innerText = api.description;
     // pricing
-    document.getElementById('price-1').innerText = api.pricing[0].plan + api.pricing[0].price;
-    document.getElementById('price-2').innerText = api.pricing[1].plan + api.pricing[1].price;
-    document.getElementById('price-3').innerText = api.pricing[2].plan;
+    document.getElementById('price-1').innerText = api.pricing[0].plan + api.pricing ? [0].price:'Free of Cost';
+    document.getElementById('price-2').innerText = api.pricing[1].plan + api.pricing ? [1].price : 'Free of Cost' ;
+    document.getElementById('price-3').innerText = api.pricing ? [2].plan : 'Go return';
     // features
     document.getElementById('features-1').innerText = api.features["1"].feature_name;
     document.getElementById('features-2').innerText = api.features["2"].feature_name;
     document.getElementById('features-3').innerText = api.features["3"].feature_name;
     // integraitions
-    document.getElementById('integration-1').innerText = api.integrations[0];
-    document.getElementById('integration-2').innerText = api.integrations[1];
-    document.getElementById('integration-3').innerText = api.integrations[2];
+    document.getElementById('integration-1').innerText = api.integrations ? [0] :'No Found';
+    document.getElementById('integration-2').innerText = api.integrations ? [1] : 'No Found';
+    document.getElementById('integration-3').innerText = api.integrations ? [2]: 'No Found';
     // right part
     console.log(api.logo)
     const rightPart = document.getElementById('right-part');
@@ -75,12 +82,12 @@ const OpenApiDetails = api =>{
     imgForRightPart.classList.add('div-right-inter')
     imgForRightPart.innerHTML = `
                   <img src="${api.image_link[0]}" alt="">
-                     <p class="accuracy">${api.accuracy['score'] * 100} % accuracy</p>
+                      <p class="accuracy">${api.accuracy['score'] * 100} % accuracy</p>
                   </img>
                   <h2 id="heading-1"></h2>
                   <p id="heading-2"></p>
             `
     rightPart.appendChild(imgForRightPart);      
-    document.getElementById('heading-1').innerText = api.input_output_examples[0].input;
-    document.getElementById('heading-2').innerText = api.input_output_examples[1].output;
+    document.getElementById('heading-1').innerText = api.input_output_examples?[0] .input : 'No! Nothing is here';
+    document.getElementById('heading-2').innerText = api.input_output_examples ? [1].output : 'No! Not yet! Take a Break Bro';
 }
